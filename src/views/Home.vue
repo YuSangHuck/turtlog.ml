658c967f97a489557b31f14e2bc3845c866ec46b
YuSangHuck <g01099387636@gmail.com>
2019-07-13 18:03:55 +0900

    test ogtag

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,16 +13,16 @@
 export default {
   name: 'Home',
   // OpenGraph
-  metaInfo: {
-    meta: [
-      { charset: 'utf-8' },
-      { property: 'og:title', content: 'home title', vmid: 'og:title' },
-      { property: 'og:description', content: 'home description', vmid: 'og:description' },
-      { property: 'og:image:type', content: 'image/jpg', vmid: 'og:image:type' },
-      { property: 'og:image', content: '/og_home.jpg', vmid: 'og:image' },
-      { property: 'og:url', content: 'http://turtlog.ml', vmid: 'og:url' },
-      { property: 'og:type', content: 'website', vmid: 'og:type' },
-    ],
-  },
+  // metaInfo: {
+  //   meta: [
+  //     { charset: 'utf-8' },
+  //     { property: 'og:title', content: 'home title', vmid: 'og:title' },
+  //     { property: 'og:description', content: 'home description', vmid: 'og:description' },
+  //     { property: 'og:image:type', content: 'image/jpg', vmid: 'og:image:type' },
+  //     { property: 'og:image', content: '/og_home.jpg', vmid: 'og:image' },
+  //     { property: 'og:url', content: 'http://turtlog.ml', vmid: 'og:url' },
+  //     { property: 'og:type', content: 'website', vmid: 'og:type' },
+  //   ],
+  // },
 };
 </script>
