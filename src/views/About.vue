999a8f5c9f6801ced5aabbbbf186b13ed0759976
YuSangHuck <g01099387636@gmail.com>
2019-07-13 17:43:28 +0900

    Add og-tag on home, about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,21 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+  // OpenGraph
+  metaInfo: {
+    meta: [
+      { charset: 'utf-8' },
+      { property: 'og:title', content: 'about title', vmid: 'og:title' },
+      { property: 'og:description', content: 'about description', vmid: 'og:description' },
+      { property: 'og:image:type', content: 'image/jpg', vmid: 'og:image:type' },
+      { property: 'og:image', content: '/og_about.jpg', vmid: 'og:image' },
+      { property: 'og:url', content: 'http://turtlog.ml', vmid: 'og:url' },
+      { property: 'og:type', content: 'website', vmid: 'og:type' },
+    ],
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,25 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+// import { Component, Vue } from 'vue-property-decorator';
 
-@Component({
-  components: {
+// @Component({
+//   components: {
+//   },
+// })
+export default {
+  name: 'Home',
+  // OpenGraph
+  metaInfo: {
+    meta: [
+      { charset: 'utf-8' },
+      { property: 'og:title', content: 'home title', vmid: 'og:title' },
+      { property: 'og:description', content: 'home description', vmid: 'og:description' },
+      { property: 'og:image:type', content: 'image/jpg', vmid: 'og:image:type' },
+      { property: 'og:image', content: '/og_home.jpg', vmid: 'og:image' },
+      { property: 'og:url', content: 'http://turtlog.ml', vmid: 'og:url' },
+      { property: 'og:type', content: 'website', vmid: 'og:type' },
+    ],
   },
-})
-export default class Home extends Vue {}
+};
 </script>
